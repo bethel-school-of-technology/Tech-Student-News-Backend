bfddae3a4dd2477ad6733183d6e8698075bda43c
Yannick Akakm Ze-ABBAYAHINSPIRED <48994548+ABBAYAHINSPIRED@users.noreply.github.com>
2020-10-31 14:07:06 -0400

    Update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -8,6 +8,12 @@ router.get("/", function(req, res, next) {
     .catch(error => res.status(500).send(error));
 });
 
+router.get("/", function(req, res, next) {
+  ArticlesModel.findAll()
+    .then(result => res.send(result))
+    .catch(error => res.status(500).send(error));
+});
+
 router.post("/", function(req, res, next) {
   console.log(req.body);
   let newArticles = new ArticlesModel();
