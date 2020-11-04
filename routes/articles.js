f8b61a0c3fce1573eb20a45a41d1271fa4861bb2
Yannick Akakm Ze-ABBAYAHINSPIRED <48994548+ABBAYAHINSPIRED@users.noreply.github.com>
2020-11-03 22:13:24 -0500

    routes/articles.js
    Updates

--- routes/articles.js
+++ routes/articles.js
@@ -3,35 +3,40 @@ var router = express.Router();
 let Articles = require("../models/article");
 
 router.get("/", function(req, res, next) {
-  Articles.find()
+  let newArticles = Articles(); 
+  newArticles.find()
   .then(articles => res.json(articles))
-  .catch(_err => res.status(404).json({ noarticlesfound: 'No articles found' }));
+  .catch(error => res.status(404).send( error  ));
 });
 
 router.get('/:id', (req, res) => {
-  Article.findById(req.params.id)
-    .then(article => res.json(article))
-    .catch(_err => res.status(404).json({ noarticlefound: 'No Article found' }));
+  let newArticles = Articles(); 
+  newArticles.findById(req.params.id)
+    .then(result => res.json(result))
+    .catch(error => res.status(404).send( error ));
 });
 
 router.post('/', (req, res) => {
-  Article.create(req.body)
-    .then(article => res.json({ msg: 'Article added successfully' }))
-    .catch(_err => res.status(400).json({ error: 'Unable to add this book' }));
+  let newArticles = Articles(); 
+     newArticles.create(req.body)
+    .then(result => res.json( result ))
+    .catch(error => res.status(400).send(error));
 });
 
 router.put('/:id', (req, res) => {
-  Artcle.findByIdAndUpdate(req.params.id, req.body)
-    .then(article => res.json({ msg: 'Updated successfully' }))
-    .catch(_err =>
-      res.status(400).json({ error: 'Unable to update the Database' })
+  let newArticles = Articles(); 
+  newArticles.findByIdAndUpdate(req.params.id, req.body)
+    .then(result => res.send(result ))
+    .catch(error =>
+      res.status(400).json(error)
     );
 });
 
 router.delete('/:id', (req, res) => {
-  Article.findByIdAndRemove(req.params.id, req.body)
-    .then(articles => res.json({ mgs: 'Article entry deleted successfully' }))
-    .catch(err => res.status(404).json({ error: 'No such a Article' }));
+  let newArticles = Articles(); 
+  newArticles.findByIdAndRemove(req.params.id, req.body)
+    .then(result => res.json(result))
+    .catch(error => res.status(404).send( error));
 });
 
 
